3f61488d326764db8dbef65e2415dc6e208c9830
nquanghien <nquanghien97@gmail.com>
2025-10-30 23:52:56 +0700

    dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image'
 import "react-datepicker/dist/react-datepicker.css";
 import PhoneIcon from './assets/PhoneIcon'
 import { ArrowBigDown, ArrowDownToLine } from 'lucide-react'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Công ty Aurora Live',
@@ -23,6 +24,34 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth scroll-pt-16 lg:scroll-pt-20">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+
+        <Script
+          id="fb-pixel-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+
         {children}
         <Analytics />
         <div className="fixed bottom-4 right-4 z-[100]">
