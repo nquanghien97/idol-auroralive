7e9775c21ce23c9c32fb71c265a7c2c11f4e0433
nquanghien <nquanghien97@gmail.com>
2025-11-08 12:38:12 +0700

    dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -21,9 +20,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const pixelId = process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID;
   return (
     <html lang="en" className="scroll-smooth scroll-pt-16 lg:scroll-pt-20">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+      <head>
         {/* google pixel */}
         <Script
           id="fb-pixel-script"
@@ -43,6 +43,38 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* TikTok Pixel */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject = t; var ttq = w[t] = w[t] || [];
+              ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "upload"];
+              ttq.setAndDefer = function (t, e) {
+                t[e] = function () {
+                  t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
+                }
+              };
+              for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
+              ttq.instance = function (t) {
+                for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++
+                ) ttq.setAndDefer(e, ttq.methods[n]); return e
+              };
+              ttq.load = function (e, n) {
+                var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                ttq._i = ttq._i || {}; ttq._i[e] = [];
+                ttq._i[e]._u = i; ttq._t = ttq._t || {}; ttq._t[e] = +new Date;
+                ttq._o = ttq._o || {}; ttq._o[e] = n || {};
+                var o = document.createElement("script"); o.type = "text/javascript"; o.async = !0; o.src = i + "?sdkid=" + e + "&lib=" + t;
+                var a = document.getElementsByTagName("script")[0];
+                a.parentNode.insertBefore(o, a)
+              };
+              ttq.load('${pixelId}');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
+      </head>
+      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <noscript>
           <img
             height="1"
@@ -53,7 +85,7 @@ export default function RootLayout({
         </noscript>
 
         {children}
-        <Analytics />
+        {/* <Analytics /> */}
         <div className="fixed bottom-4 right-4 z-[100]">
           <Link href="tel:0867059120" className="w-16 h-16 rounded-full bg-[#1877f2] flex justify-center items-center mb-4 shadow-custom">
             <PhoneIcon width={40} height={40} fill='white' />
